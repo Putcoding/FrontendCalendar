8db4b9609659da175d0897aecfc3927cf4334769
Jakub Domagała <falonix01@gmail.com>
2023-04-17 10:23:37 +0200

    Changed router's providing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import './App.css';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import "./App.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Login from "./Pages/Login";
 import Calendar from "./Pages/Calendar";
-window.localStorage.setItem("login_key",0);
+import RootLayout from "./Pages/Root";
+window.localStorage.setItem("login_key", 0);
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <Login />,
+      },
+      { path: "login", element: <Login /> },
+      { path: "calendar", element: <Calendar /> },
+    ],
+  },
+]);
+
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/Calendar" element={<Calendar />}/>
-      </Routes>
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
