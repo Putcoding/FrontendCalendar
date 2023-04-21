cf4de88837ffdb01048f46d30d3b72b4a81e814d
Jakub Domagała <falonix01@gmail.com>
2023-04-21 09:41:48 +0200

    Added date display for each item, now in console.log()

--- src/Components/PlannerItem.js
+++ src/Components/PlannerItem.js
@@ -1,8 +1,17 @@
+import { useEffect } from "react";
+import classes from "./PlannerItem.module.css";
+
 const PlannerItem = (props) => {
+  const currentDate = props.currentDate;
+  const dayOffset = props.offset;
+  let componentDate = new Date();
+  componentDate.setDate(currentDate.getDate() + dayOffset);
+  console.log(componentDate);
+
   return (
-    <>
+    <div className={classes["planner-item"]}>
       <h2>{props.day}</h2>
-    </>
+    </div>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import AuthContext, { AuthContextProvider } from "./store/auth";
+import { AuthContextProvider } from "./store/auth";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
